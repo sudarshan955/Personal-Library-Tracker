381325be90db027d5eaeaa4f9aa745239b858aa9
Sudarshan Biswas <sudarshan955@gmail.com>
2016-03-17 21:58:48 +0600

    added add new book layout

---
+++ app/src/main/java/com/adlabsbd/anup/personal_library_tracker/AddNewBookActivity.java
@@ -0,0 +1,13 @@
+package com.adlabsbd.anup.personal_library_tracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddNewBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_book);
+    }
+}
