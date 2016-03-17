ea8549253a4d8230fde6331957f3d6747b990043
anupdey99 <anupdey99@gmail.com>
2016-03-17 11:48:12 +0600

    Splash Screen Add!

---
+++ app/src/main/java/com/adlabsbd/anup/personal_library_tracker/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.adlabsbd.anup.personal_library_tracker;
+
+import android.app.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        /*
+         * Showing splash screen with a timer. This will be useful when you
+         * want to show case your app logo / company
+         */
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                // This method will be executed once the timer is over
+                // Start your app main activity
+
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
+
+    }
+}
